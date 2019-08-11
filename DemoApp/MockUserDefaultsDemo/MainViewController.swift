bef7576754be8d2f2e7920051cb7b4199432fdf4
417.72KI <417.72ki@gmail.com>
2019-08-11 19:38:35 +0900

    Rename

--- DemoApp/MockUserDefaultsDemo/MainViewController.swift
+++ DemoApp/MockUserDefaultsDemo/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MockUserDefaultsDemo
 //
 //  Created by 417.72KI on 2019/07/07.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
