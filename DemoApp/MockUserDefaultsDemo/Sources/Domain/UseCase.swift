1982bcdaa6562adcb2a1e1e4a4b6e5547db6046f
417-72KI <417.72ki@gmail.com>
2022-03-31 15:22:00 +0900

    remove RxSwift codes from UseCase

--- DemoApp/MockUserDefaultsDemo/Sources/Domain/UseCase.swift
+++ DemoApp/MockUserDefaultsDemo/Sources/Domain/UseCase.swift
@@ -7,33 +7,17 @@
 //
 
 import Foundation
-import RxSwift
 
 protocol UsesUseCase {
     var useCase: UseCase { get }
 }
 
 protocol UseCase: UsesUserDefaults {
-    @available(*, deprecated, message: "Use concurrency")
-    func fetchAll() -> Single<[Model]>
     func fetchAll() async -> [Model]
     func save(_ model: Model)
 }
 
 extension UseCase {
-    func fetchAll() -> Single<[Model]> {
-        return Single.create { [userDefaults] event in
-            let dictionary = userDefaults.dictionaryRepresentation()
-                // Exclude System keys
-                .filter { !$0.key.starts(with: "NS") && !$0.key.starts(with: "Apple") }
-                .filter { !["AddingEmojiKeybordHandled",
-                            "PKKeychainVersionKey",
-                            "AKLastIDMSEnvironment"].contains($0.key) }
-            event(.success(dictionary.mapValues { "\($0)" }.map(Model.init).sorted()))
-            return Disposables.create()
-        }
-    }
-
     func fetchAll() async -> [Model] {
         userDefaults.dictionaryRepresentation()
             .filter { !$0.key.starts(with: "NS") && !$0.key.starts(with: "Apple") } // Exclude System keys
