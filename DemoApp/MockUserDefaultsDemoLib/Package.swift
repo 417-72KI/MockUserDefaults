00ae84db1809983686c4ca1ea0aa0a2517c73fcd
417-72KI <417.72ki@gmail.com>
2023-09-17 02:36:36 +0900

    bump swift-tools-version to 5.6

--- DemoApp/MockUserDefaultsDemoLib/Package.swift
+++ DemoApp/MockUserDefaultsDemoLib/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MockUserDefaultsDemoLib",
     defaultLocalization: "ja",
-    platforms: [.macOS(.v11), .iOS(.v15)],
+    platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)],
     products: [
         .library(
             name: "MockUserDefaultsDemoLib",
